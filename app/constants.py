55714da461d0bb700d8cd83b5cb416b8ae04c851
Markus <mapmarkus@gmail.com>
2022-02-22 17:59:32 +0000

    Add session max age

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,9 @@
+from datetime import timedelta
+
 # Constants used accross the app
-#
+
+# Store key namespace
 STORE_KEY = 'session.id.{session_id}'
+
+# Session max age
+SESSION_MAX_AGE = timedelta(hours=24)
